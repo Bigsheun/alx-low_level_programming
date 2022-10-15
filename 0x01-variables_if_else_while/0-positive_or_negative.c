32737d15b3ca3b419498a6678303b37742b4b438
Bigsheun <bigsheun1812@gmail.com>
2022-10-14 19:10:43 -0700

    project-x01

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+* main - c-program entry point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d %s\n", n, "is positive");
+	if (n == 0)
+		printf("%d %s\n", n, "is zero");
+	if (n < 0)
+		printf("%d %s\n", n, "is negative");
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,7 @@
+# 0x01-variables_if_else_while
+
+- __[GitHub Account](github.com/Bigsheun)__ - high quality and fast image resize in browser.
+
+ ![Picture of Bigsheun](https://avatars.githubusercontent.com/u/88635898?s=120&v=4 "Bigsheun")
+___
+
