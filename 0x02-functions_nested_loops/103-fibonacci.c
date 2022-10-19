be8892e8da5c4f0c3f280b49cd845fa611d419f3
Bigsheun <bigsheun1812@gmail.com>
2022-10-19 10:13:34 -0700

    extra-4

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: success
+*/
+int main(void)
+{
+	int i;
+	unsigned long int a, b, c, d;
+
+	a = 1;
+	b = 2;
+	d = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (a < 4000000 && (a % 2) == 0)
+		{
+			d = d + a;
+		}
+		c = a + b;
+		a = b;
+		b = c;
+	}
+
+	printf("%lu\n", d);
+
+	return (0);
+}
+
