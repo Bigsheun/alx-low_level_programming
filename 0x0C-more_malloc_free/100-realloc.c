2a06fa243bbbf5ddc878562be75a15148726abe1
Bigsheun1812 <bigsheun1812@gmail.com>
2022-11-11 03:31:28 +0100

    'extrases'

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 
 	tmp = ptr;
-	N = (new_size > old_size)? old_size : new_size;
+	N = (new_size > old_size) ? old_size : new_size;
 
 	for (i = 0; i < N; i++)
 	{
