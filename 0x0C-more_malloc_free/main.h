676728d97aaddc6e39cf703bc4d8660cf8c78bfb
Bigsheun1812 <bigsheun1812@gmail.com>
2022-11-11 03:27:23 +0100

    'extrases'

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmedb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int c_atoi(char x)
+int is_number(char *num_str);
+void *mul_array(char *a1, int len1, char a2, char *a3, int lena);
+void print_array(char *a, int nb);
 #endif
 
